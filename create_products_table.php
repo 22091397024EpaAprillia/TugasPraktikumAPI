66ca95be09c5a1c184080e6b6ca1d81229ff70e2
22091397024_Epa Aprillia <124507843+22091397024EpaAprillia@users.noreply.github.com>
2024-03-15 15:25:58 +0700

    Update create_products_table.php

--- create_products_table.php
+++ create_products_table.php
@@ -14,11 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
-            $table->text('cover')->nullable();
-            $table->foreignId('category_id')->nullable()->constrained()->cascadeOnDelete();
+           $table->id();
+            $table->string('nama');
+            $table->string('nim')->unique();
+            $table->string('Prodi')->unique();
+            $table->foreignId('mahasiswa_id')->nullable()->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
