59b7fd16902c96b81efbc56745fd2939030f1c3f
22091397024_Epa Aprillia <124507843+22091397024EpaAprillia@users.noreply.github.com>
2024-03-15 15:32:05 +0700

    Update adminSeeder.php

--- adminSeeder.php
+++ adminSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 
-class AdminSeeder extends Seeder
+class MahasiswaSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,16 +15,29 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $admin = [
-                [
-                    'username' => 'admin',
-                    'email' => 'admin@example.com',
-                    'password' => bcrypt('123'),
-                    'is_admin' => true,
-                ]
-            ];
-            
-        User::insert($admin);
+        $mahasiswas = [
+            [
+                'username' => 'mahasiswa1',
+                'email' => 'mahasiswa1@example.com',
+                'password' => bcrypt('password1'),
+                'is_admin' => false,
+            ],
+            [
+                'username' => 'mahasiswa2',
+                'email' => 'mahasiswa2@example.com',
+                'password' => bcrypt('password2'),
+                'is_admin' => false,
+            ],
+            // Tambahkan data mahasiswa lainnya sesuai kebutuhan
+        ];
 
+        foreach ($mahasiswas as $mahasiswa) {
+            User::create([
+                'username' => $mahasiswa['username'],
+                'email' => $mahasiswa['email'],
+                'password' => $mahasiswa['password'],
+                'is_admin' => $mahasiswa['is_admin'],
+            ]);
+        }
     }
 }
